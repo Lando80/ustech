35ca5c297e173daff7f63f24f33143022989eaef
Rickson Lima <lima.rickson20@gmail.com>
2021-10-18 00:28:56 -0400

    sei la mano

--- src/components/Chat/index.js
+++ src/components/Chat/index.js
@@ -1,71 +1,72 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import { Widget, addResponseMessage } from "react-chat-widget";
 import "react-chat-widget/lib/styles.css";
 import "./style.css";
 
-class Chat extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: "",
-      gender: "",
-      data_nacimento: "",
-      cpf: "",
-      email: "",
-      telefone: "",
-      rg: "",
-      cep: "",
-    };
-  }
+import api from "../../services/api";
 
-  componentDidMount() {
-    addResponseMessage("Olá, eu sou a Mia.");
-    addResponseMessage("Qual é o seu nome?");
-  }
+function Chat() {
+  const [nome, setNome] = useState("");
+  const [cpf, setCpf] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [cep, setCep] = useState("");
 
-  handleNewUserMessage = (newMessage) => {
-    this.$nome = newMessage;
-    if ((this.$nome = !"")) {
-      addResponseMessage("Olá, " + newMessage);
+  const [index, setIndex] = useState(0);
+  const [response, setResponse] = useState("");
+
+  const createContributor = async () => {
+    try {
+      const res = await api.post("/contributor", { nome, cpf });
+      alert(res.data.message);
+    } catch (error) {
+      alert(error.response.data.message);
     }
-
-    switch (newMessage) {
-      case "1":
-        addResponseMessage("OláLucas Cavalcante " + newMessage);
-        addResponseMessage("Rodrigo Carvalho");
-        addResponseMessage("Magvinier Jr");
-        addResponseMessage("Dinara Araújo");
-        addResponseMessage("Digite 2 para acessar o menu de opções.");
-        break;
-      case "2":
-        addResponseMessage("Para saber a profissão de cada um, Digite 3");
-        addResponseMessage("Para Finalizar acesso, Digite 5");
-        break;
-      case "3":
-        addResponseMessage("Lucas: Desenvolvedor Web");
-        addResponseMessage("Rodrigo: Engenheiro de Testes");
-        addResponseMessage("Dinara: Engenheria de Testes");
-        addResponseMessage("Magvinier: Bancário");
-        addResponseMessage("Para Finalizar acesso, Digite 5");
-        break;
-      case "5":
-        alert("Obrigado Pelo Acesso ");
-        break;
-    }
-
-    console.log(newMessage);
   };
 
-  render() {
-    return (
-      <div className="App">
-        <Widget
-          handleNewUserMessage={this.handleNewUserMessage}
-          title="MIA"
-          subtitle="Sua Assistente Virtual"
-        />
-      </div>
-    );
-  }
+  const updateContributor = (path) => {
+    console.log(path);
+  };
+
+  useEffect(() => {
+    addResponseMessage("Olá, eu sou a Mia.");
+    addResponseMessage("Qual é o seu nome?");
+  }, []);
+
+  useEffect(() => {
+    if (cpf) createContributor();
+    if (email) updateContributor("email");
+    if (phone) updateContributor("phone");
+    if (cep) updateContributor("cep");
+  }, [index]);
+
+  const handleNewUserMessage = (newMessage) => {
+    if (index === 0) {
+      setNome(newMessage);
+      addResponseMessage("Olá, " + newMessage);
+      addResponseMessage("Informe seu CPF");
+    }
+
+    if (index === 1) {
+      setCpf(newMessage);
+    }
+
+    if (index === 2) {
+      addResponseMessage(response);
+    }
+
+    setIndex(index + 1);
+  };
+
+  return (
+    <div className="App">
+      <Widget
+        handleNewUserMessage={handleNewUserMessage}
+        title="MIA"
+        subtitle="Sua Assistente Virtual"
+      />
+    </div>
+  );
 }
+
 export default Chat;
