a6a2e78c6c3a98ce06cf1673d2a0ede204377d26
Rickson Lima <lima.rickson20@gmail.com>
2021-10-18 02:31:41 -0400

    issue: cpf executando mais de uma vez

--- src/components/Chat/index.js
+++ src/components/Chat/index.js
@@ -12,20 +12,41 @@ function Chat() {
   const [phone, setPhone] = useState("");
   const [cep, setCep] = useState("");
 
+  const [_id, setId] = useState("");
+
   const [index, setIndex] = useState(0);
   const [response, setResponse] = useState("");
 
   const createContributor = async () => {
     try {
-      const res = await api.post("/contributor", { nome, cpf });
-      alert(res.data.message);
+      const res = await api.post("/contributor", { nome });
+      setResponse(res.data.message);
+      setId(res.data._id);
     } catch (error) {
-      alert(error.response.data.message);
+      setResponse(error.response.data.message);
     }
   };
 
-  const updateContributor = (path) => {
-    console.log(path);
+  const updateContributor = async (key, value) => {
+    try {
+      let obj = {};
+      obj[key] = value;
+
+      let res;
+      if (key === "cpf") {
+        console.log("CPF executado");
+        obj["_id"] = _id;
+        res = await api.put(`/contributor/${key}`, obj);
+      } else {
+        obj["cpf"] = cpf;
+        res = await api.put(`/contributor/${key}`, obj);
+        console.log(`${key} executado`);
+      }
+
+      setResponse(res.data.message);
+    } catch (error) {
+      setResponse(error.response.data.message);
+    }
   };
 
   useEffect(() => {
@@ -34,25 +55,37 @@ function Chat() {
   }, []);
 
   useEffect(() => {
-    if (cpf) createContributor();
-    if (email) updateContributor("email");
-    if (phone) updateContributor("phone");
-    if (cep) updateContributor("cep");
+    console.log(index);
+    if (nome) createContributor();
+    if (cpf && index === 2) updateContributor("cpf", cpf);
+    if (email && index === 3) updateContributor("email", email);
+    if (phone && index === 4) updateContributor("phone", phone);
+    if (cep && index === 5) updateContributor("cep", cep);
   }, [index]);
+
+  useEffect(() => {
+    if (response) {
+      addResponseMessage(response);
+      console.log("Response: " + response);
+    }
+  }, [response]);
 
   const handleNewUserMessage = (newMessage) => {
     if (index === 0) {
       setNome(newMessage);
       addResponseMessage("Olá, " + newMessage);
-      addResponseMessage("Informe seu CPF");
     }
-
     if (index === 1) {
       setCpf(newMessage);
     }
-
     if (index === 2) {
-      addResponseMessage(response);
+      setEmail(newMessage);
+    }
+    if (index === 3) {
+      setPhone(newMessage);
+    }
+    if (index === 4) {
+      setCep(newMessage);
     }
 
     setIndex(index + 1);
