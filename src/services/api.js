afbdf3c8009ab86d5b81d1213cc8d988c0985b84
Rickson Lima <lima.rickson20@gmail.com>
2021-10-20 17:48:22 -0400

    changed the axios baseURL to deployed api

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getToken } from "./auth";
 
 const api = axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL: "https://api-mia.herokuapp.com/api",
 });
 
 api.interceptors.request.use(async (config) => {
